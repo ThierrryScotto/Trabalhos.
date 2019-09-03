71fd41f5b48a649efe976d6df9b78a2ba261b34f
Thierry <37625040+ThierrryScotto@users.noreply.github.com>
2019-09-03 17:43:26 -0300

    Add files via upload

---
+++ TesteFile/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TesteFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entrada_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.verificar_button = new System.Windows.Forms.Button();
+            this.saida_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // entrada_textBox
+            // 
+            this.entrada_textBox.Location = new System.Drawing.Point(12, 49);
+            this.entrada_textBox.Name = "entrada_textBox";
+            this.entrada_textBox.Size = new System.Drawing.Size(146, 20);
+            this.entrada_textBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Arquivo ou Diretório";
+            // 
+            // verificar_button
+            // 
+            this.verificar_button.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verificar_button.Location = new System.Drawing.Point(200, 46);
+            this.verificar_button.Name = "verificar_button";
+            this.verificar_button.Size = new System.Drawing.Size(75, 31);
+            this.verificar_button.TabIndex = 3;
+            this.verificar_button.Text = "Verificar";
+            this.verificar_button.UseVisualStyleBackColor = true;
+            this.verificar_button.Click += new System.EventHandler(this.Verificar_button_Click);
+            // 
+            // saida_textBox
+            // 
+            this.saida_textBox.Location = new System.Drawing.Point(1, 106);
+            this.saida_textBox.Multiline = true;
+            this.saida_textBox.Name = "saida_textBox";
+            this.saida_textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.saida_textBox.Size = new System.Drawing.Size(379, 179);
+            this.saida_textBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 286);
+            this.Controls.Add(this.saida_textBox);
+            this.Controls.Add(this.verificar_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.entrada_textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox entrada_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button verificar_button;
+        private System.Windows.Forms.TextBox saida_textBox;
+    }
+}
+
---
+++ TesteFile/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TesteFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Verificar_button_Click(object sender, EventArgs e)
+        {
+            //Limpa a saída
+            saida_textBox.Text = null;
+
+            //verifica se o nome do arquivo existe
+            if (File.Exists(entrada_textBox.Text + ".txt"))
+            {
+                //variavel responsavel por guardar o nome do arquivo ou diretorio com seu formato adequado
+                string pastaOuArquivo = entrada_textBox.Text + ".txt";
+
+                //retorna a hora em que o arquivo foi criado
+                saida_textBox.Text += "Hora da criação: " + File.GetCreationTime(pastaOuArquivo) + "\n\n";
+
+                //retorna informaçãoes de quando o arquivo teve um ultimo acesso
+                saida_textBox.Text += "Utimo acesso" + File.GetLastAccessTimeUtc(pastaOuArquivo) + "\n\n";
+
+                try
+                {
+                    //retorna o conteudo dentro do arquivo
+                    StreamReader leitura = new StreamReader(pastaOuArquivo);
+                    //metodo lé o arquivo até o final
+                    saida_textBox.Text += leitura.ReadToEnd();
+                }
+
+                catch (IOException erro)
+                {
+                    MessageBox.Show(erro.Message);
+                }
+            }
+            else if(Directory.Exists(entrada_textBox.Text))
+            {
+                //recebe os subdiretorio de um determinado diretorio
+                string[] subdiretorios;
+
+                try
+                {
+                    //exibe a data e a hora em que o diretorio foi criado
+                    saida_textBox.Text += Directory.GetCreationTime(entrada_textBox.Text);
+
+                    //retorna os subdiretorios do diretorio especificado
+                    subdiretorios = Directory.GetDirectories(entrada_textBox.Text);
+
+                    foreach (var nomes in subdiretorios)
+                        saida_textBox.Text += nomes + "\n";
+                }
+                catch (IOException erro)
+                {
+                    MessageBox.Show(erro.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não existe arquivo ou diretorio com este nome" + "Erro" + MessageBoxIcon.Error);
+            }
+                
+
+        }
+    }
+}
